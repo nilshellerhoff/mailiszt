0cda4bbf173c226a99eed58b11706022da780b40
nilshellerhoff <nils.hellerhoff@gmail.com>
2021-10-05 22:53:55 +0200

    fixed groups with new mailbox

--- frontend/src/views/mailboxes/AddMailbox.vue
+++ frontend/src/views/mailboxes/AddMailbox.vue
@@ -4,6 +4,8 @@
     :popupTitle="`Add mailbox`"
     :btnState="btnState"
     :mailbox="mailbox"
+    :groupsAvail="groupsAvail"
+    :groups="groups"
   >
   </MailboxPopup>
 </template>
@@ -17,12 +19,36 @@ export default {
     return {
       mailbox: {},
       btnState: "ready",
+      groupsAvail: [
+        {
+          text: "Group",
+          value: "i_group",
+          type: "item",
+          items: [{ text: "", value: "" }],
+        },
+      ],
+      groups: {
+        id: 1631607982497,
+        comparisonOperator: "=",
+        value: 1,
+        entity: "i_group",
+        logicOperator: "none",
+        arguments: [],
+      },
     };
   },
   components: {
     MailboxPopup,
   },
   methods: {
+    getGroupsAvail() {
+      this.$api.get(`group`).then((response) => {
+        this.groupsAvail[0].items = response.data.map((group) => ({
+          value: group.i_group,
+          text: group.s_name,
+        }));
+      });
+    },
     async saveMailbox() {
       this.btnState = "loading";
       this.$api.put(`/mailbox/add`, this.mailbox).then(() => {
@@ -33,6 +59,9 @@ export default {
         }, 500);
       });
     },
+  },
+  mounted() {
+    this.getGroupsAvail();
   },
 };
 </script>
