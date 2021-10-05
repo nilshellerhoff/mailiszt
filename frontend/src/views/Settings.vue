67047d6cbabca36bef84710900287f18a307b728
nilshellerhoff <nils.hellerhoff@gmail.com>
2021-10-05 22:44:10 +0200

    added cronjob url

--- frontend/src/views/Settings.vue
+++ frontend/src/views/Settings.vue
@@ -31,21 +31,29 @@
             :message="newPasswordError ? 'passwords do not match' : ''"
           ></PasswordInput>
         </v-col>
-    </v-row>
-    <v-row>
-      <v-col cols=3>
-        <v-btn @click="updatePassword">change password!</v-btn>
-      </v-col>
+        <v-col cols=3>
+          <v-btn @click="updatePassword">change password!</v-btn>
+        </v-col>
     </v-row>
 
     <h2>asdfsadf</h2>
     <h3>Cron-Job</h3>
-    point your cron-job to the following url: 
+    <v-row>
+      <v-col cols="4">point your cron-job to the following url:</v-col>
+      <v-col cols="5"><v-text-field disabled="true" v-model="cronUrl"></v-text-field></v-col>
+      <v-col cols="3">
+        <v-btn @click="urlCopy">
+          <v-icon>mdi-content-copy</v-icon>
+          copy
+        </v-btn>
+      </v-col>
+    </v-row>
   </div>
 </template>
 
 <script>
 import PasswordInput from '@/components/PasswordInput.vue'
+import copy from 'copy-to-clipboard'
 
 export default {
   name: "Settings",
@@ -58,6 +66,7 @@ export default {
     newPassword2: '',
     oldPasswordError: false,
     newPasswordError: false,
+    cronUrl: process.env.VUE_APP_BASE_URL + '/mailbox/forward'
   }),
   methods: {
     updatePassword() {
@@ -84,6 +93,9 @@ export default {
       this.$refs.oldPassword.password = '';
       this.$refs.newPassword1.password = '';
       this.$refs.newPassword2.password = '';
+    },
+    urlCopy() {
+      copy(this.cronUrl);
     }
   }
 };
