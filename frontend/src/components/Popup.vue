0897455647906d9f35552379e6da514db15ac797
nilshellerhoff <nils.hellerhoff@gmail.com>
2021-10-20 12:29:23 +0200

    added Popup

---
+++ frontend/src/components/Popup.vue
@@ -0,0 +1,34 @@
+
+<template>
+  <v-dialog v-model="dialog" :max-width="width" :style="{ zIndex: zIndex }" @keydown.esc="cancel">
+    <v-card>
+      <v-toolbar dark color="secondary" dense flat>
+        <v-btn icon dark @click="dialog = false">
+          <v-icon>mdi-close</v-icon>
+        </v-btn>
+        <v-toolbar-title class="white--text">{{ title }}</v-toolbar-title>
+      </v-toolbar>
+      <v-card-text class="pa-4">
+        <slot></slot>
+      </v-card-text>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  props: {
+    title: String,
+    width: {type: Number, default: 300},
+    zIndex: {type: Number, default: 200},
+  },
+  data: () => ({
+    dialog: false,
+  }),
+  methods: {
+    open() {
+      this.dialog = true
+    },
+  }
+}
+</script>
