2e4c622e4c8cf09aec178236612525e1ebce0522
nilshellerhoff <nils.hellerhoff@gmail.com>
2021-10-21 17:10:10 +0200

    formatting

--- frontend/src/views/Settings.vue
+++ frontend/src/views/Settings.vue
@@ -1,16 +1,14 @@
 <template>
   <div>
     <h1>Settings</h1>
-    <h2>User</h2>
-    <h3>Change password</h3>
+    <h2>Change password</h2>
     <change-password></change-password>
 
 
-    <h2>asdfsadf</h2>
-    <h3>Cron-Job</h3>
+    <h2>Cron-Job</h2>
     <v-row>
       <v-col cols="4">point your cron-job to the following url:</v-col>
-      <v-col cols="5"><v-text-field disabled v-model="cronUrl"></v-text-field></v-col>
+      <v-col cols="5"><v-text-field disabled dense single-line v-model="cronUrl"></v-text-field></v-col>
       <v-col cols="3">
         <v-btn @click="urlCopy">
           <v-icon left>mdi-content-copy</v-icon>
