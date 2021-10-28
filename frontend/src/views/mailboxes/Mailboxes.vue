6ae46feaf1bf533ef46e0ad2999db2db00a7db86
nilshellerhoff <nils.hellerhoff@gmail.com>
2021-10-28 14:18:29 +0200

    revamped search fields

--- frontend/src/views/mailboxes/Mailboxes.vue
+++ frontend/src/views/mailboxes/Mailboxes.vue
@@ -1,25 +1,51 @@
 <template>
-  <v-container class="m-10">
+  <v-container class="ma-0 pa-1 pa-sm-4" fluid>
     <v-row no-gutters>
-      <v-col sm="3" cols="6">
-        <h1>Mailboxes</h1>
-      </v-col>
-      <v-col sm="3" cols="6" align="right" order="1" order-sm="2" class="pa-2">
-        <v-btn color="primary" to="/mailboxes/add">
-          <v-icon class="mr-2">mdi-plus</v-icon>
-          Add new
-        </v-btn>
-      </v-col>
-      <v-col sm="6" cols="12" order="2" order-sm="1" class="mt-n1">
+      <v-col class="d-none d-sm-flex">
         <v-text-field
           v-model="search"
-          append-icon="mdi-magnify"
+          prepend-icon="mdi-magnify"
+          clearable
           label="Search"
-          single-line
           hide-details
+          autocomplete="false"
+        ></v-text-field>
+      </v-col>
+      <v-col cols=auto class="py-2 pl-2">
+        <!-- search button (only on xs) -->
+        <v-btn
+          class="mx-1 d-sm-none pa-2 pa-sm-4"
+          :color="mobileSearchVisible ? 'grey lighten-2' : 'grey lighten-4'"
+          @click="mobileSearchVisible = !mobileSearchVisible"
+        ><v-icon>mdi-magnify</v-icon>search
+        </v-btn>
+        <!-- add new button -->
+        <v-btn
+          class="mx-1 pa-2 pa-sm-4"
+          color="primary"
+          to="/mailboxes/add"
+          >
+          <v-icon class="mr-2">mdi-plus</v-icon>
+          add new
+        </v-btn>
+      </v-col>
+    </v-row>
+
+    <!-- search field on mobile -->
+    <v-row no-gutters v-show="mobileSearchVisible" class="d-sm-none">
+      <v-col>
+        <v-text-field
+          v-model="search"
+          prepend-icon="mdi-magnify"
+          clearable
+          label="Search"
+          hide-details
+          autocomplete="false"
         ></v-text-field>
       </v-col>
     </v-row>
+
+    <!-- table -->
     <v-data-table :headers="headers" :items="mailboxes" :search="search">
       <!-- edit link -->
       <template v-slot:[`item.actions`]="{ item }">
@@ -49,6 +75,7 @@ export default {
       { text: "E-Mail address", value: "s_address" },
       { text: "", value: "actions", sortable: false, align: "right" },
     ],
+    mobileSearchVisible: false,
   }),
   methods: {
     getMailboxes() {
--- frontend/src/views/mails/Mails.vue
+++ frontend/src/views/mails/Mails.vue
@@ -1,21 +1,42 @@
 <template>
-  <v-container class="m-10">
+  <v-container class="ma-0 pa-1 pa-sm-4" fluid>
     <v-row no-gutters>
-      <v-col sm="3" cols="6">
-        <h1>Mails</h1>
-      </v-col>
-      <v-col sm="3" cols="6" align=right order="1" order-sm="2" class=pa-2>
-      </v-col>
-      <v-col sm="6" cols="12" order="2" order-sm="1" class=mt-n1>
+      <v-col class="d-none d-sm-flex">
         <v-text-field
           v-model="search"
-          append-icon="mdi-magnify"
+          prepend-icon="mdi-magnify"
+          clearable
           label="Search"
-          single-line
           hide-details
+          autocomplete="false"
+        ></v-text-field>
+      </v-col>
+      <v-col cols=auto class="py-2 pl-2">
+        <!-- search button (only on xs) -->
+        <v-btn
+          class="mx-1 d-sm-none pa-2 pa-sm-4"
+          :color="mobileSearchVisible ? 'grey lighten-2' : 'grey lighten-4'"
+          @click="mobileSearchVisible = !mobileSearchVisible"
+        ><v-icon>mdi-magnify</v-icon>search
+        </v-btn>
+      </v-col>
+    </v-row>
+
+    <!-- search field on mobile -->
+    <v-row no-gutters v-show="mobileSearchVisible" class="d-sm-none">
+      <v-col>
+        <v-text-field
+          v-model="search"
+          prepend-icon="mdi-magnify"
+          clearable
+          label="Search"
+          hide-details
+          autocomplete="false"
         ></v-text-field>
       </v-col>
     </v-row>
+
+    <!-- table -->
     <v-data-table :headers="headers" :items="mails" :search="search">
       <!-- details button -->
       <template v-slot:[`item.actions`]="{ item }">
@@ -36,6 +57,7 @@ export default {
       { text: "Subject", value: "s_subject" },
       { text: "", value: "actions", sortable: false, align: "right"}
     ],
+    mobileSearchVisible: false,
   }),
   methods: {
     getMails() {
