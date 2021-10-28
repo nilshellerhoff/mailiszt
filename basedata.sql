1d1185289cbfc1d274e7ccea2d0eea9833e95816
nilshellerhoff <nils.hellerhoff@gmail.com>
2021-10-28 15:55:49 +0200

    comments

--- basedata.sql
+++ basedata.sql
@@ -2,6 +2,6 @@
 INSERT INTO setting (s_name, s_type, v_value)
 VALUES ('version_number', 'string', '0.1')
 
--- insert the user
+-- insert the default user (username: admin, pw: admin)
 INSERT INTO user (s_username, s_password, s_role)
 VALUES ('admin', '$2y$10$zqpp/tiCrXc1bEN3tCklxuLJchle1JSzU26jtusLlJqsoVugr2IK.', 'ADMIN')
