2595ec4d2e0228a5bb6e2befdc4e1d377782a7ca
nilshellerhoff <nils.hellerhoff@gmail.com>
2021-10-28 15:54:44 +0200

    added default user

--- basedata.sql
+++ basedata.sql
@@ -1,2 +1,7 @@
+-- insert the version number
 INSERT INTO setting (s_name, s_type, v_value)
 VALUES ('version_number', 'string', '0.1')
+
+-- insert the user
+INSERT INTO user (s_username, s_password, s_role)
+VALUES ('admin', '$2y$10$zqpp/tiCrXc1bEN3tCklxuLJchle1JSzU26jtusLlJqsoVugr2IK.', 'ADMIN')
