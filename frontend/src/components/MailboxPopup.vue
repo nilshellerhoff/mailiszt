5470d3548e51516f0af75faa4acae232922fb4da
nilshellerhoff <nils.hellerhoff@gmail.com>
2021-11-05 21:14:19 +0100

    added tabs

--- frontend/src/components/MailboxPopup.vue
+++ frontend/src/components/MailboxPopup.vue
@@ -5,98 +5,116 @@
     :popupTitle="popupTitle"
     closeUrl="/mailboxes"
   >
-    <v-text-field
-      prepend-icon="mdi-form-textbox"
-      label="Name"
-      v-model="mailbox.s_name"
-    ></v-text-field>
-    <v-text-field
-      prepend-icon="mdi-at"
-      label="Email Address"
-      v-model="mailbox.s_address"
-    ></v-text-field>
-    <v-row no-gutters>
-      <v-col cols="8" class="pr-2">
-        <v-text-field
-          prepend-icon="mdi-email-receive"
-          label="IMAP server"
-          v-model="mailbox.s_imapserver"
-        ></v-text-field>
-      </v-col>
-      <v-col cols="4">
-        <v-text-field
-          prepend-icon=""
-          label="IMAP port"
-          v-model="mailbox.n_imapport"
-        ></v-text-field>
-      </v-col>
-    </v-row>
-    <v-row no-gutters>
-      <v-col cols="8" class="pr-2">
-        <v-text-field
-          prepend-icon="mdi-email-send"
-          label="SMTP server"
-          v-model="mailbox.s_smtpserver"
-        ></v-text-field>
-      </v-col>
-      <v-col cols="4">
-        <v-text-field
-          prepend-icon=""
-          label="SMTP port"
-          v-model="mailbox.n_smtpport"
-        ></v-text-field>
-      </v-col>
-    </v-row>
-    <v-row no-gutters>
-      <v-col cols="6" class="pr-2">
-        <v-text-field
-          prepend-icon="mdi-account"
-          label="Username"
-          v-model="mailbox.s_username"
-        ></v-text-field>
-      </v-col>
-      <v-col cols="6">
-        <v-text-field
-          v-model="mailbox.s_password"
-          :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
-          :type="showPassword ? 'text' : 'password'"
-          label="Password"
-          @click:append="showPassword = !showPassword"
-        ></v-text-field>
-      </v-col>
-    </v-row>
-    <div class="pa-4">
-      <BooleanInput
-        ref="boolInp"
-        @change="getSql()"
-        :entities="groupsAvail"
-        :condition="groups"
-      ></BooleanInput>
-    </div>
+    <!-- tabs list -->
+    <v-tabs v-model="currentTab">
+      <v-tab>server</v-tab>
+      <v-tab>groups</v-tab>
+    </v-tabs>
 
-    <v-btn @click="openRecipientsPopup()">Show recipients</v-btn>
-    <Popup
-      ref="recipientsPopup"
-      :title="`Recipients for ${mailbox.s_name}`"
-      :width="500"
-    >
-      <v-chip-group column>
-        <div v-for="r in recipients" :key="r.i_member">
-          <router-link :to="`/members/edit/${r.i_member}`">
-            <v-chip>
-              {{ r.s_name1 + " " + r.s_name2 }}
-            </v-chip>
-          </router-link>
+    <!-- server configuration -->
+    <v-tabs-items v-model="currentTab">
+      <v-tab-item>
+        <v-text-field
+          prepend-icon="mdi-form-textbox"
+          label="Name"
+          v-model="mailbox.s_name"
+        ></v-text-field>
+        <v-text-field
+          prepend-icon="mdi-at"
+          label="Email Address"
+          v-model="mailbox.s_address"
+        ></v-text-field>
+        <v-row no-gutters>
+          <v-col cols="8" class="pr-2">
+            <v-text-field
+              prepend-icon="mdi-email-receive"
+              label="IMAP server"
+              v-model="mailbox.s_imapserver"
+            ></v-text-field>
+          </v-col>
+          <v-col cols="4">
+            <v-text-field
+              prepend-icon=""
+              label="IMAP port"
+              v-model="mailbox.n_imapport"
+            ></v-text-field>
+          </v-col>
+        </v-row>
+        <v-row no-gutters>
+          <v-col cols="8" class="pr-2">
+            <v-text-field
+              prepend-icon="mdi-email-send"
+              label="SMTP server"
+              v-model="mailbox.s_smtpserver"
+            ></v-text-field>
+          </v-col>
+          <v-col cols="4">
+            <v-text-field
+              prepend-icon=""
+              label="SMTP port"
+              v-model="mailbox.n_smtpport"
+            ></v-text-field>
+          </v-col>
+        </v-row>
+        <v-row no-gutters>
+          <v-col cols="6" class="pr-2">
+            <v-text-field
+              prepend-icon="mdi-account"
+              label="Username"
+              v-model="mailbox.s_username"
+            ></v-text-field>
+          </v-col>
+          <v-col cols="6">
+            <v-text-field
+              v-model="mailbox.s_password"
+              :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+              :type="showPassword ? 'text' : 'password'"
+              label="Password"
+              @click:append="showPassword = !showPassword"
+            ></v-text-field>
+          </v-col>
+        </v-row>
+      </v-tab-item>
+
+      <!-- group/user configuration -->
+      <v-tab-item>
+
+        <!-- group selector -->
+        <span class="text-subtitle-2">Groups selector</span>
+        <div class="pa-4">
+          <BooleanInput
+            ref="boolInp"
+            @change="getSql()"
+            :entities="groupsAvail"
+            :condition="groups"
+          ></BooleanInput>
         </div>
-      </v-chip-group>
-    </Popup>
+
+        <v-btn @click="openRecipientsPopup()">Show recipients</v-btn>
+        <Popup
+          ref="recipientsPopup"
+          :title="`Recipients for ${mailbox.s_name}`"
+          :width="500"
+        >
+          <v-chip-group column>
+            <div v-for="r in recipients" :key="r.i_member">
+              <router-link :to="`/members/edit/${r.i_member}`">
+                <v-chip>
+                  {{ r.s_name1 + " " + r.s_name2 }}
+                </v-chip>
+              </router-link>
+            </div>
+          </v-chip-group>
+        </Popup>
+      </v-tab-item>
+    </v-tabs-items>
   </DetailsPopup>
 </template>
 
 <style scoped>
-  a {
-    text-decoration: none;
-  }
+a {
+  text-decoration: none;
+}
 </style>
 
 <script>
@@ -112,6 +130,7 @@ export default {
       showPassword: false,
       sql: "",
       recipients: null,
+      currentTab: null,
     };
   },
   components: {
