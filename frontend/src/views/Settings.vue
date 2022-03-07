5e233fcc28ffc0aa5005147f89af2b229bb8c3c0
nilshellerhoff <nils.hellerhoff@gmail.com>
2022-03-07 16:38:24 +0100

    added to frontend

--- frontend/src/views/Settings.vue
+++ frontend/src/views/Settings.vue
@@ -7,16 +7,8 @@
 
 
     <span class="text-h6">Cron-Job</span><br>
-    <span class="text-subtitle-1">Call the following URL periodically in order check for new mails</span>
-    <v-row>
-      <v-col cols="5"><v-text-field disabled dense single-line v-model="cronUrl"></v-text-field></v-col>
-      <v-col cols="3">
-        <v-btn @click="urlCopy">
-          <v-icon left>mdi-content-copy</v-icon>
-          copy
-        </v-btn>
-      </v-col>
-    </v-row>
+    <cronjob></cronjob>
+
 
     <span class="text-h6">DB access</span><br>
     <span class="text-subtitle-1">Access the DB directly via a SQL prompt. URL is <code>&lt;host&gt;/dbadmin</code>.
@@ -26,25 +18,20 @@
 </template>
 
 <script>
-import copy from 'copy-to-clipboard'
 import ChangePassword from '@/components/Settings/ChangePassword.vue'
 import EnableDbadmin from '@/components/Settings/EnableDbadmin.vue'
+import Cronjob from '@/components/Settings/Cronjob.vue'
 
 export default {
   name: "Settings",
   components: {
     ChangePassword,
-    EnableDbadmin
+    EnableDbadmin,
+    Cronjob,
   },
   data: () => ({
-    cronUrl: process.env.VUE_APP_BASE_URL + '/mailbox/forward',
     versionNumber: ''
   }),
-  methods: {
-    urlCopy() {
-      copy(this.cronUrl);
-    },
-  },
   mounted() {
     this.$root.$on('reloadData', () => {
       this.$api.get(`/setting/version_number`).then(response => {
