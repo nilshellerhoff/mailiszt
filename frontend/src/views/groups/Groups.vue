28ca61208714238fbcc554efc8faa733bc5ead9a
nilshellerhoff <nils.hellerhoff@gmail.com>
2022-05-02 19:48:55 +0200

    added mailto links

--- frontend/src/views/groups/Groups.vue
+++ frontend/src/views/groups/Groups.vue
@@ -47,11 +47,51 @@
 
     <!-- table -->
     <v-data-table :headers="headers" :items="groups" :search="search" :options="{ itemsPerPage: -1 }">
-      <!-- edit link -->
       <template v-slot:[`item.actions`]="{ item }">
+        <!-- mailto link -->
+        <v-tooltip bottom>
+          <template v-slot:activator="{ on, attrs }">
+            <v-icon
+              v-bind="attrs"
+              v-on="on"
+              class="mr-2"
+              @click="copyMailtoString(item.i_group)"
+            >mdi-content-copy</v-icon>
+          </template>
+          <span>copy email addresses</span>
+        </v-tooltip>
+
+        <v-tooltip bottom>
+          <template v-slot:activator="{ on, attrs }">
+            <a
+              :href="`mailto:${getMailtoString(item.i_group)}`"
+              v-bind="attrs"
+              v-on="on"
+              >
+              <v-icon class="mr-2">mdi-email-edit</v-icon>
+            </a>
+          </template>
+          <span>send email to group members</span>
+        </v-tooltip>
+
+        <v-tooltip bottom>
+          <template v-slot:activator="{ on, attrs }">
+            <a
+              :href="`mailto:?bcc=${getMailtoString(item.i_group)}`"
+              v-bind="attrs"
+              v-on="on"
+              >
+              <v-icon class="mr-2">mdi-email-edit-outline</v-icon>
+            </a>
+          </template>
+          <span>send email to group members (BCC)</span>
+        </v-tooltip>
+
+        <!-- edit link -->
         <router-link :to="'/groups/edit/' + item.i_group">
           <v-btn small class="mr-2">Edit</v-btn>
         </router-link>
+        <!-- edit link -->
         <v-btn small color="error" @click="deleteGroup(item.i_group, item.s_name)">Delete</v-btn>
       </template>
 
@@ -66,6 +106,8 @@
 </style>
 
 <script>
+import copy from "copy-to-clipboard";
+
 export default {
   name: "Home",
   data: () => ({
@@ -92,6 +134,12 @@ export default {
             })
           }
         });
+    },
+    getMailtoString(i_group) {
+      return this.groups.filter(g => g.i_group == i_group)[0].members.map(m => m.s_email).join(',');
+    },
+    copyMailtoString(i_group) {
+      copy(this.getMailtoString(i_group))
     }
   },
   mounted() {
