78973c1bc54e8e69fcce97bd46b38a36f8c84bed
nilshellerhoff <nils.hellerhoff@gmail.com>
2022-05-20 13:19:21 +0200

    show attachments in browser window

--- api/attachments.php
+++ api/attachments.php
@@ -9,7 +9,7 @@ Route::add('/api/attachment/([0-9a-f_]*)', function($attachment_file) {
         $db = new DB();
         $data = $db->queryRow("SELECT * FROM attachment WHERE s_filename = ?", [$attachment_file]);
         header('Content-type: ' . $data['s_contenttype']);
-        header('Content-Disposition: attachment; filename="' . $data['s_name'] . '"');
+        header('Content-Disposition: inline; filename="' . $data['s_name'] . '"');
         return readfile(ATTACHMENT_PATH . $attachment_file);
     }, $attachment_file);
 }, 'GET');
