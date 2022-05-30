a90ecb034372a0900fa42939a1a74265823ea64a
nilshellerhoff <nils.hellerhoff@gmail.com>
2022-05-30 21:30:03 +0200

    add templating functoin for mail

--- classes/Mail.php
+++ classes/Mail.php
@@ -264,4 +264,16 @@ class Mail extends Base {
 
         return $mail;
     }
+
+    /** Format a used for mail template
+     * @param $template string template to format where {{field}} is replaced by the value of the field
+     * @param $fields array fields to replace in the template
+     * @return string formatted template
+     */
+    public static function formatTemplate($template, $fields) {
+        foreach ($fields as $key => $value) {
+            $template = str_replace("{{$key}}", $value, $template);
+        }
+        return $template;
+    }
 }
