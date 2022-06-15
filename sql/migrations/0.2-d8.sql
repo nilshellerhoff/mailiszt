cf5dabdba73640515bab7e9fda34638250aa6df0
nilshellerhoff <nils.hellerhoff@gmail.com>
2022-06-15 13:21:50 +0200

    workaround for DEFAULT in body template

--- sql/migrations/0.2-d8.sql
+++ sql/migrations/0.2-d8.sql
@@ -5,5 +5,10 @@ ALTER TABLE mailbox
 ADD s_templatefrom TEXT NOT NULL DEFAULT '{{fromname}} via {{listname}}';
 
 -- x'0a' is SQLITE for newline character
+-- ALTER TABLE mailbox
+-- ADD s_templatebody TEXT NOT NULL DEFAULT '{{body}}' || x'0a' || x'0a' || '----------------' || x'0a' || 'This mail was originally sent by {{frommail}} to {{listaddress}}. Contact the moderator at {{moderatoraddress}} regarding any questions.';
+
+-- temporary workaround without footer because the concatenate operator || doesn't work in DEFAULT clause
+-- (see https://stackoverflow.com/questions/72622175/add-column-with-default-including-newlines)
 ALTER TABLE mailbox
-ADD s_templatebody TEXT NOT NULL DEFAULT '{{body}}' || x'0a' || x'0a' || '----------------' || x'0a' || 'This mail was originally sent by {{frommail}} to {{listaddress}}. Contact the moderator at {{moderatoraddress}} regarding any questions.';
+ADD s_templatebody TEXT NOT NULL DEFAULT '{{body}}'
